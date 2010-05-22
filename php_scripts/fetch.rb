53820db95b23e44ba2f6afff7d9959dbbd0163fa
John Murray <murrayj5@nku.edu>
2010-05-22 16:09:35 -0400

    added code for modx

--- php_scripts/fetch.rb
+++ php_scripts/fetch.rb
@@ -19,6 +19,7 @@ BASE_URL = Array.new()
 BASE_URL[0] = 'http://squirrelmail.org/plugins_category.php?category_id=all'
 BASE_URL[1] = 'http://wordpress.org/extend/plugins/browse/new/page/'
 BASE_URL[2] = 'http://www.mediawiki.org'
+BASE_URL[3] = 'http://modxcms.com/extras/search.html?query=&limit=0'
 FILENAME = 'pluginStats.csv'
 ERROR_FILE = 'fetch_errors.txt'
 
@@ -30,22 +31,24 @@ ERROR_FILE = 'fetch_errors.txt'
 ##-----------------------------------------------------------------------------
 def main()
 
-	#check flags from command line (currently statically implemented)
-	page_to_crawl = BASE_URL[2]
+    #check flags from command line (currently statically implemented)
+    page_to_crawl = BASE_URL[3]
 
-	#create file for reporting to
-	write_headers_to_file()
+    #create file for reporting to
+    write_headers_to_file()
 
-	#determine function to call for page crawling
-	case page_to_crawl
-		when BASE_URL[0]
-			fetch_squirrelmail()
-		when BASE_URL[1]
-			fetch_wordpress(BASE_URL[1])
+    #determine function to call for page crawling
+    case page_to_crawl
+        when BASE_URL[0]
+            fetch_squirrelmail(BASE_URL[0])
+        when BASE_URL[1]
+            fetch_wordpress(BASE_URL[1])
         when BASE_URL[2]
             fetch_media_wiki(BASE_URL[2])
-	end
-	
+        when BASE_URL[3]
+            fetch_modx(BASE_URL[3])
+    end
+
 end
 
 
@@ -54,7 +57,7 @@ end
 ## FUNCTION: fetch_squirrelmail
 ## PURPOSE: fetch plugins from squirrelmail and write to file
 ##-----------------------------------------------------------------------------
-def fetch_squirrelmail()
+def fetch_squirrelmail(base_url)
 end
 
 
@@ -65,84 +68,84 @@ end
 ##-----------------------------------------------------------------------------
 def fetch_wordpress(base_url)
 
-	#Fetch the total number of plugin pages from wordpress' website
-	num_of_plugin_pages = 0
-	agent = Mechanize.new
-	page = agent.get(base_url + '1/')
-	page.links.each do |link|
-		if link.href =~ /extend\/plugins\/browse\/new\/page\//
-			if link.text.to_i > 500
-				num_of_plugin_pages = link.text.to_i
-			end
-		end
-	end
+    #Fetch the total number of plugin pages from wordpress' website
+    num_of_plugin_pages = 0
+    agent = Mechanize.new
+    page = agent.get(base_url + '1/')
+    page.links.each do |link|
+        if link.href =~ /extend\/plugins\/browse\/new\/page\//
+            if link.text.to_i > 500
+                num_of_plugin_pages = link.text.to_i
+            end
+        end
+    end
 
-	#loop through all of the pages and get all of the needed data
-	(1..num_of_plugin_pages).each do |i|
-		#define a browser agent for searching web pages
-		agent = :Mechanize.new
-		page0 = agent.get(base_url + i.to_s + "/")
-		page0.links.each do |link0|
+    #loop through all of the pages and get all of the needed data
+    (1..num_of_plugin_pages).each do |i|
+        #define a browser agent for searching web pages
+        agent = :Mechanize.new
+        page0 = agent.get(base_url + i.to_s + "/")
+        page0.links.each do |link0|
 
-			next if link0.href =~ /\/extend\/plugins\/tags\// ||
-				link0.href =~ /\/extend\/plugins\/...about\// ||
-				link0.href =~ /\/extend\/plugins\/browse\/.+\//
+            next if link0.href =~ /\/extend\/plugins\/tags\// ||
+                link0.href =~ /\/extend\/plugins\/...about\// ||
+                link0.href =~ /\/extend\/plugins\/browse\/.+\//
 
-			#verify that we are following the right links
-			if link0.href =~ /\/extend\/plugins\/[a-z0-9_\-]{1,}/
+            #verify that we are following the right links
+            if link0.href =~ /\/extend\/plugins\/[a-z0-9_\-]{1,}/
 
-				#get and verify the link
-				begin
-					page1 = agent.get(link0.href)
-				rescue Exception => e
-					puts "\t\tcould not get file"
-					puts "\t\t" + e.message
-					next
-				end
+                #get and verify the link
+                begin
+                    page1 = agent.get(link0.href)
+                rescue Exception => e
+                    puts "\t\tcould not get file"
+                    puts "\t\t" + e.message
+                    next
+                end
 
-				#collect individual plugin data
-				page1.links.each do |link1|
-					if link1.href =~ /\.zip/
-						#try to get the file and collect data
-						begin
-							#download file
-							pluginPage = agent.get(link1.href)
-							wgetFile = File.new(File.basename(link1.href), 'w')
-							wgetFile.write(pluginPage.body)
-							wgetFile.close
+                #collect individual plugin data
+                page1.links.each do |link1|
+                    if link1.href =~ /\.zip/
+                        #try to get the file and collect data
+                        begin
+                            #download file
+                            pluginPage = agent.get(link1.href)
+                            wgetFile = File.new(File.basename(link1.href), 'w')
+                            wgetFile.write(pluginPage.body)
+                            wgetFile.close
 
-							#write information to the results array
-							results= Array.new(4)
-							temp_page_title = page1.title[12, page1.title.length - 1]
-							results[0] = temp_page_title.split("\u00AB")[0] #plugin name
-							results[2] = link0.href #url
-							pluginSplit = link1.href.split('/')
-							results[3] = pluginSplit[pluginSplit.length - 1] #file name
+                            #write information to the results array
+                            results= Array.new(4)
+                            temp_page_title = page1.title[12, page1.title.length - 1]
+                            results[0] = temp_page_title.split("\u00AB")[0] #plugin name
+                            results[2] = link0.href #url
+                            pluginSplit = link1.href.split('/')
+                            results[3] = pluginSplit[pluginSplit.length - 1] #file name
 
-							#get version
-							hp = Hpricot(page1.body).search("li")
-							hp.each do |item|
-								if item.innerText =~ /Version:/
-									results[1] = item.innerText[9, item.innerText.length - 1] #version
-									break
-								end
-							end
+                            #get version
+                            hp = Hpricot(page1.body).search("li")
+                            hp.each do |item|
+                                if item.innerText =~ /Version:/
+                                    results[1] = item.innerText[9, item.innerText.length - 1] #version
+                                    break
+                                end
+                            end
 
-							#write out results to file
-							write_to_file(results)
+                            #write out results to file
+                            write_to_file(results)
 
-						rescue Exception => e
-							errorFile = File.new("fetch_errors.txt", "a")
-							errorFile.write( "Plugin Name: " + page1.title + "\n" )
-							errorFile.write( "\t=> " + link1.href + "\n" )
-							errorFile.write( "\t=> " + e.message + "\n\n")
-							errorFile.close
-						end
-					end
-				end
-			end
-		end
-	end
+                        rescue Exception => e
+                            errorFile = File.new("fetch_errors.txt", "a")
+                            errorFile.write( "Plugin Name: " + page1.title + "\n" )
+                            errorFile.write( "\t=> " + link1.href + "\n" )
+                            errorFile.write( "\t=> " + e.message + "\n\n")
+                            errorFile.close
+                        end
+                    end
+                end
+            end
+        end
+    end
 end
 
 
@@ -317,11 +320,76 @@ end
 
 
 
+def fetch_modx(base_url)
+
+    agent = Mechanize.new
+    agent.user_agent = Repository::CHROME_USER_AGENT
+
+    outer_page = agent.get(base_url)
+
+    #get an array of all the pages
+    outer_page.links.each do |link|
+
+        if link.href =~ /extras\/package\//
+
+            inner_agent = Mechanize.new
+            inner_agent.user_agent = Repository::CHROME_USER_AGENT
+            inner_page = inner_agent.get('http://www.modxcms.com/' + link.href)
+
+            results = Array.new(4)
+
+            #set filename to empty
+            results[3] = 'N/A'
+
+            inner_page.links.each do |inner_link|
+
+                if inner_link.href =~ /extras\/download\//
+
+                    #if there is a file, record it
+                    #and downlaod file
+                    download_page = inner_agent.get( 'http://www.modxcms.com/' + inner_link.href)
+                    download_page.links.each do |dl|
+                        if dl.href =~ /extras\/dl.html\?file=/
+                            results[3] = inner_link.text.to_s.gsub(/^[\n ]+/, '')
+                            wgetfile = File.new( inner_link.text.to_s.gsub(/^[\n ]+/, ''), 'wb')
+                            dl = inner_agent.get(dl.href)
+                            wgetfile.write( dl.body )
+                            wgetfile.close
+                        end
+                    end
+
+                end
+
+            end #end inner page links
+
+            #get version
+            h = Hpricot( inner_page.body )
+            results[1] = h.search("//span[@class='rm-version']").inner_text
+
+            #get name
+            title = inner_page.title
+            title = title.gsub('Package: ', '')
+            results[0] = title.split('|')[0]
+
+            #get URL
+            results[2] = link.href
+
+            write_to_file(results)
+
+        end #end inner page
+
+    end #end outer page links
+
+end
+
+
+
+
 def write_to_file(row)
 
-	file = File.new(FILENAME, "a")
-	file.write(row[0] + ',' + row[1] + ',' + row[2] + ',' + row[3] + ',' + "\n")
-	file.close
+    file = File.new(FILENAME, "a")
+    file.write(row[0] + ',' + row[1] + ',' + row[2] + ',' + row[3] + ',' + "\n")
+    file.close
 
 end
 
@@ -329,9 +397,9 @@ end
 
 def write_headers_to_file()
 
-	file = File.new(FILENAME, "w")
-	file.write('Plugin Name, Version, Url, File Name,,' + "\n")
-	file.close
+    file = File.new(FILENAME, "w")
+    file.write('Plugin Name, Version, Url, File Name,,' + "\n")
+    file.close
 
 end
 
@@ -340,10 +408,10 @@ end
 #does nothing for now
 def get_command_line_arguments()
 
-	args = Array.new
-	ARGV.each do |a|
-		file.write()
-	end
+    args = Array.new
+    ARGV.each do |a|
+        file.write()
+    end
 
 end
 
