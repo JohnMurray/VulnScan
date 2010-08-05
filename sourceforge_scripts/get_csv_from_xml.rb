3e4726680af08cf18ee3cd1ac03a6dd84b3988fa
John Murray <murrayj5@nku.edu>
2010-08-05 17:30:45 -0400

    made minor modifications to fix syntactical and logical errors, no logic really changed though

--- sourceforge_scripts/get_csv_from_xml.rb
+++ sourceforge_scripts/get_csv_from_xml.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'hpricot'
-
+require 'progressbar'
 
 XML_FILE = 'sourceforge_data.xml'
 CSV_FILE = 'sourceforge_data.csv'
 
 #load in the file to parse into an Hpricot Element
 begin
-    xml = Hpricot( File.open(XML_FIL) )
+    xml = Hpricot( File.open(XML_FILE) )
 rescue
     Fail "Could not open data file for proccessing"
 end
@@ -22,18 +22,22 @@ end
 
 
 #try to start iterating throught the file and getting all download counts
-(h/:project).each do |p|
+pbar = ProgressBar.new("xml2csv", (xml/:project).each.count)
+(xml/:project).each do |p|
     name = p.attributes['name']
     url = p.attributes['url']
     total = 0
     
     #iterate through items and get total download count
     (p/:item).each do |it|
-        total += (it/:download)[0].gsub(/[ ,]+/, '').to_i
+        total += (it/:downloads)[0].inner_html.to_s.gsub(/[ ,]+/, '').to_i
     end
 
     #write to csv file
     File.open(CSV_FILE, 'a') {|f|
-        f.write(name + ',' + url + ',' + total + ',');
+        f.write(name + ',' + url + ',' + total.to_s + ',');
+        f.write("\n");
     }
+    pbar.inc
 end
+pbar.finish
